15310ea04e906ba38808100b3aeed7246ff555ac
Slepov Vladimir <slepvladimir@yandex.ru>
2019-02-14 09:51:14 +0300

    fix_2 creat isValid method.

--- src/Main.java
+++ src/Main.java
@@ -6,22 +6,30 @@ public class Main {
 
     private static final Pattern PATTERN_LETTER = Pattern.compile("[А-Я]|[а-я]|[A-Z]|[a-z]|[\\(\\)\\~\\@\\#\\$\\%\\^\\&\\_\\=\\.\\,\\?\\:\\;\\'\\|]");
     private static final Pattern START_END_SYMBOL = Pattern.compile("^\\D|\\D$");
+
+
     public static void main(String[] arg){
 
         Scanner in = new Scanner(System.in);
         while (true){
             String input= in.nextLine();
-            Matcher isCorrect = PATTERN_LETTER.matcher(input);
-            Matcher isSymbol = START_END_SYMBOL.matcher(input);
-            if(isCorrect.find() || isSymbol.find()){
-                System.out.println("Sorry, the string is wrong.");
-            }
-            else{
+            if(isValid(input)){
                 Calculate task0 = new Calculate(input);
                 System.out.println(task0.getResult());
+            }
+            else{
+                System.out.println("Sorry, the string is wrong.");
             }
 
         }
 
+    }
+
+
+    private static boolean isValid(String str){
+        str = str.trim();
+        Matcher isCorrect = PATTERN_LETTER.matcher(str);
+        Matcher isSymbol = START_END_SYMBOL.matcher(str);
+        return !(isCorrect.find() || isSymbol.find());
     }
 }
